6c5e3da08bbd5e6a6d05a0d8362acbab93b7e5db
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-07 22:10:17 +0100

    Making main produce the output to use pdftk

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ import book.Book;
 import book.Signature;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 
 public class Main {
     public static void main(String[] args) {
-        final var book = new Book(List.of(Signature.withNumberOfSheets(3).get(),
+        final var book = new Book(List.of(
+                Signature.withNumberOfSheets(3).get(),
                 Signature.withNumberOfSheets(4).get(),
                 Signature.withNumberOfSheets(4).get(),
                 Signature.withNumberOfSheets(4).get(),
@@ -14,9 +16,12 @@ public class Main {
                 Signature.withNumberOfSheets(4).get(),
                 Signature.withNumberOfSheets(3).get()));
         final var pages = book.getTotalNumberOfPages();
+        final var output = IntStream.range(0,pages)
+                .map(i->book.getPageNumber(i))
+                .map(i->i+1)
+                .mapToObj(i->String.valueOf(i))
+                .reduce("",(acc,x)->acc+" "+x);
         System.out.println("pages : " + pages);
-        for (int i = 0; i < pages; ++i) {
-            System.out.print(book.getPageNumber(i)+" ");
-        }
+        System.out.println("pdftk.exe binding.pdf cat "+output+" output out.pdf");
     }
 }
