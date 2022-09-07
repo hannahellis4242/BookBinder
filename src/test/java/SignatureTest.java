3b9f8306283ab706a1a70196a061e13745419be1
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-07 15:36:20 +0100

    Adding tests for 4 sheet signatures

--- src/test/java/SignatureTest.java
+++ src/test/java/SignatureTest.java
@@ -29,13 +29,15 @@ class SignatureTest {
 
     private static Stream<Arguments> testPageParamsProvider() {
         return Stream.of(
+                //out of bound tests
                 Arguments.of(1, -1, Optional.empty()),
+                Arguments.of(1, 4, Optional.empty()),
+                //real values for a one sheet signature
                 Arguments.of(1, 0, Optional.of(3)),
                 Arguments.of(1, 1, Optional.of(0)),
                 Arguments.of(1, 2, Optional.of(1)),
                 Arguments.of(1, 3, Optional.of(2)),
-                Arguments.of(1, 4, Optional.empty()),
-
+//real values for a two sheet signature
                 Arguments.of(2, 0, Optional.of(7)),
                 Arguments.of(2, 1, Optional.of(0)),
                 Arguments.of(2, 2, Optional.of(1)),
@@ -43,7 +45,37 @@ class SignatureTest {
                 Arguments.of(2, 4, Optional.of(5)),
                 Arguments.of(2, 5, Optional.of(2)),
                 Arguments.of(2, 6, Optional.of(3)),
-                Arguments.of(2, 7, Optional.of(4))
+                Arguments.of(2, 7, Optional.of(4)),
+//real values for a three sheet signature
+                Arguments.of(3, 0, Optional.of(11)),
+                Arguments.of(3, 1, Optional.of(0)),
+                Arguments.of(3, 2, Optional.of(1)),
+                Arguments.of(3, 3, Optional.of(10)),
+                Arguments.of(3, 4, Optional.of(9)),
+                Arguments.of(3, 5, Optional.of(2)),
+                Arguments.of(3, 6, Optional.of(3)),
+                Arguments.of(3, 7, Optional.of(8)),
+                Arguments.of(3, 8, Optional.of(7)),
+                Arguments.of(3, 9, Optional.of(4)),
+                Arguments.of(3, 10, Optional.of(5)),
+                Arguments.of(3, 11, Optional.of(6)),
+//real values for a four sheet signature
+                Arguments.of(4, 0, Optional.of(15)),
+                Arguments.of(4, 1, Optional.of(0)),
+                Arguments.of(4, 2, Optional.of(1)),
+                Arguments.of(4, 3, Optional.of(14)),
+                Arguments.of(4, 4, Optional.of(13)),
+                Arguments.of(4, 5, Optional.of(2)),
+                Arguments.of(4, 6, Optional.of(3)),
+                Arguments.of(4, 7, Optional.of(12)),
+                Arguments.of(4, 8, Optional.of(11)),
+                Arguments.of(4, 9, Optional.of(4)),
+                Arguments.of(4, 10, Optional.of(5)),
+                Arguments.of(4, 11, Optional.of(10)),
+                Arguments.of(4, 12, Optional.of(9)),
+                Arguments.of(4, 13, Optional.of(6)),
+                Arguments.of(4, 14, Optional.of(7)),
+                Arguments.of(4, 15, Optional.of(8))
         );
     }
 
