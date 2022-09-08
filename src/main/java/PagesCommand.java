3b9147ea9dce7abb616fe57d06efbdb48201c519
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-08 10:36:24 +0100

    Making pages command work

--- src/main/java/PagesCommand.java
+++ src/main/java/PagesCommand.java
@@ -1,20 +1,20 @@
 import book.Book;
 import book.Signature;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 
 public class PagesCommand implements Command {
     @Override
     public void run(String[] args) {
-        final var book = new Book(List.of(
-                new Signature(3),
-                new Signature(4),
-                new Signature(4),
-                new Signature(4),
-                new Signature(4),
-                new Signature(4),
-                new Signature(3)));
+        final var signatures = Arrays.stream(args)
+                .skip(1)
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Signature::new)
+                .collect(Collectors.toList());
+        final var book = new Book(signatures);
         final var pages = book.getTotalNumberOfPages();
         final var output = IntStream.range(0, pages)
                 .map(book::getPageNumber)
