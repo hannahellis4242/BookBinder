d8aa49e3fe3817d456e07dab55207a87b30c8bb2
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-15 13:50:45 +0100

    Option builder can now build from an array

--- src/main/java/signature/OptionBuilder.java
+++ src/main/java/signature/OptionBuilder.java
@@ -2,6 +2,7 @@ package signature;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class OptionBuilder {
     Map<Integer, Integer> signatureCounts = new HashMap<>();
@@ -13,6 +14,24 @@ public class OptionBuilder {
     public OptionBuilder add(int size, int number) {
         final int count = signatureCounts.getOrDefault(size, 0);
         signatureCounts.put(size, count + number);
+        return this;
+    }
+
+    private static int getOrDefault(int index, int... arr) {
+        if (index < 0 || index >= arr.length) {
+            return 0;
+        }
+        return arr[index];
+    }
+
+    OptionBuilder fromArray(int min, int max, int... arr) {
+        if (min > max) {
+            throw new IllegalArgumentException("minimum value given is larger than the maximum value given");
+        }
+        IntStream.range(0, max - min + 1)
+                .mapToObj(i -> new int[]{i, getOrDefault(i, arr)})
+                .filter(xs -> xs[1] != 0)
+                .forEach(xs -> add(xs[0] + min, xs[1]));
         return this;
     }
 
--- src/test/java/signature/OptionBuilderTest.java
+++ src/test/java/signature/OptionBuilderTest.java
@@ -11,11 +11,7 @@ import static org.hamcrest.Matchers.equalTo;
 class OptionBuilderTest {
     @Test
     void whenAddingIndividualSizes() {
-        OptionBuilder builder = new OptionBuilder();
-        builder.add(3);
-        builder.add(4);
-        builder.add(3);
-        final var option = builder.build();
+        final var option = new OptionBuilder().add(3).add(4).add(3).build();
         assertThat(Arrays.stream(option.getSignatureSizes()).boxed().toArray(), arrayContaining(3, 4));
         assertThat(option.getNumberOfSignatureSized(3), equalTo(2));
         assertThat(option.getNumberOfSignatureSized(4), equalTo(1));
@@ -24,10 +20,7 @@ class OptionBuilderTest {
 
     @Test
     void whenAddingMultiple() {
-        OptionBuilder builder = new OptionBuilder();
-        builder.add(3, 2);
-        builder.add(4, 1);
-        final var option = builder.build();
+        final var option = new OptionBuilder().add(3, 2).add(4, 1).build();
         assertThat(Arrays.stream(option.getSignatureSizes()).boxed().toArray(), arrayContaining(3, 4));
         assertThat(option.getNumberOfSignatureSized(3), equalTo(2));
         assertThat(option.getNumberOfSignatureSized(4), equalTo(1));
@@ -36,18 +29,29 @@ class OptionBuilderTest {
 
     @Test
     void whenAddingExistingOption() {
-        OptionBuilder builder = new OptionBuilder();
-        {
-            OptionBuilder inner = new OptionBuilder();
-            inner.add(3, 2);
-            final var existingOption = inner.build();
-            builder.add(existingOption);
-        }
-        builder.add(4, 1);
-        final var option = builder.build();
+        final var existingOption = new OptionBuilder().add(3, 2).build();
+        final var option = new OptionBuilder().add(existingOption).add(4).build();
         assertThat(Arrays.stream(option.getSignatureSizes()).boxed().toArray(), arrayContaining(3, 4));
         assertThat(option.getNumberOfSignatureSized(3), equalTo(2));
         assertThat(option.getNumberOfSignatureSized(4), equalTo(1));
         assertThat(option.getTotalNumberOfPages(), equalTo(40));
+    }
+
+    @Test
+    void whenBuildingFromArray() {
+        OptionBuilder builder = new OptionBuilder();
+        final var array = new int[]{1, 0, 2, 1, 1};
+        builder.fromArray(2, 7, array);
+        final var option = builder.build();
+        assertThat(Arrays.stream(option.getSignatureSizes()).boxed().toArray(), arrayContaining(2, 4, 5, 6));
+        assertThat(option.getNumberOfSignatureSized(1), equalTo(0));
+        assertThat(option.getNumberOfSignatureSized(2), equalTo(1));
+        assertThat(option.getNumberOfSignatureSized(3), equalTo(0));
+        assertThat(option.getNumberOfSignatureSized(4), equalTo(2));
+        assertThat(option.getNumberOfSignatureSized(5), equalTo(1));
+        assertThat(option.getNumberOfSignatureSized(6), equalTo(1));
+        assertThat(option.getNumberOfSignatureSized(7), equalTo(0));
+        assertThat(option.getNumberOfSignatureSized(8), equalTo(0));
+        assertThat(option.getTotalNumberOfPages(), equalTo(84));
     }
 }
